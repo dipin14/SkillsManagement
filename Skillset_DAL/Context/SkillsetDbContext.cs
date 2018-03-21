70c89d54c97e2e09f565e71d0152071d15a31ed7
dipin14 <dipin14@gmail.com>
2018-03-21 18:12:51 +0530

    Framework Changes

---
+++ Skillset_DAL/Context/SkillsetDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Skillset_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillset_DAL.Context
+{
+    public class SkillsetDbContext : DbContext
+    {
+        public SkillsetDbContext() : base(nameOrConnectionString: "Default") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
---
+++ Skillset_DAL/Models/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillset_DAL.Models
+{
+    public class Skill
+    {
+        public int skillId;
+        public string skillName;
+        public string skillDescription;
+    }
+}
