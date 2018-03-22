4ac9388cecf13b3021aa3dc63d763548d73ac30f
Nikhal62 <nikhal.p@experionglobal.com>
2018-03-22 13:02:03 +0530

    my first commit

---
+++ Skillset_PL/Controllers/EmployeeSkillController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skillset_PL.Controllers
+{
+    public class EmployeeSkillController : Controller
+    {
+        // GET: EmployeeSkill
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
