c72239647abafaf5b63f058355a05fbc91593774
rizwan.ebrahim@experionglobal.com <rizwan.ebrahim@experionglobal.com>
2018-03-22 19:11:20 +0530

    nothing

---
+++ Skillset-PL/Controllers/EmployeeRatingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skillset_PL.Controllers
+{
+    public class EmployeeRatingController : Controller
+    {
+        private  IQuestion;
+        public EmployeeRatingController()
+        {
+
+        }
+        public EmployeeRatingController()
+        {
+
+        }
+        // GET: EmployeeRating
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+     
+        //Display all skills from the skill table
+        public ActionResult AllSkills()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Skillset-PL/Scripts/EmployeeRating.js
@@ -0,0 +1,45 @@
+﻿
+function CRateOver(id, val) {
+
+    for (var i = 1; i <= val; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starGlow';
+    }
+}
+
+function CRateOut(id, val) {
+    for (var i = 1; i <= val; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starFade';
+    }
+}
+
+function CRateSelected(id) {
+
+    var setRating = document.getElementById("Rating " + id).value
+
+    for (var i = 1; i <= setRating; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starGlow';
+    }
+}
+
+function CRate(id, val) {
+
+    document.getElementById("Rating " + id).value = val;
+
+    for (var i = 1; i <= val; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starGlow';
+    }
+    // unselect remaining
+    for (var i = val + 1; i <= 5; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starFade';
+    }
+}
