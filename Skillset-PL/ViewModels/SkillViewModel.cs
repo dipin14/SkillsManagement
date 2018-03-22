18039874f7eb54729669506b222df706d464ec6b
dipin14 <dipin14@gmail.com>
2018-03-22 11:19:58 +0530

    ADDED Npgsql Ef packages Ef latest package Services folder in BLL Repository folder in DAL ContextClass folder in DAL SkillsetDbContext class in DAL Models folder in DAL Skill model class in DAL ViewModels class in PL SkillViewModel class in PL

---
+++ Skillset-PL/ViewModels/SkillViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Skillset_PL.ViewModels
+{
+    public class SkillViewModel
+    {
+        [Key]
+        int skillId;
+        string skillName;
+        string skillDescription;
+
+        public int SkillId
+        {
+            get
+            {
+                return skillId;
+            }
+
+            set
+            {
+                skillId = value;
+            }
+        }
+
+        [Required]
+        [Display(Name = "Skill Name")]
+        public string SkillName
+        {
+            get
+            {
+                return skillName;
+            }
+
+            set
+            {
+                skillName = value;
+            }
+        }
+
+        [Required]
+        [Display(Name = "Skill Description")]
+        public string SkillDescription
+        {
+            get
+            {
+                return skillDescription;
+            }
+
+            set
+            {
+                skillDescription = value;
+            }
+        }
+    }
+}
--- Skillset_PL/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Skillset_PL.MvcApplication" Language="C#" %>
